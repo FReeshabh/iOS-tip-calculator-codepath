491bf74c739086704fc958d9ae4a55bddcd3e71c
Rishabh Tewari <rishabhtewari1@gmail.com>
2021-05-24 01:55:34 -0500

    Added the split bill feature

--- tipcalc-codepath/ViewController.swift
+++ tipcalc-codepath/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControlSlider: UISlider!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var numPeopleLabel: UILabel!
+    @IBOutlet weak var numPeopleStepper: UIStepper!
+    @IBOutlet weak var splitBill: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Tip Calculator"
         // Do any additional setup after loading the view.
+        billAmountTextField.becomeFirstResponder()
     }
 
     @IBAction func calculateTip(_ sender: Any) {
@@ -34,24 +39,33 @@ class ViewController: UIViewController {
         //Update the percentage label
         tipAmountPercentage.text = String(format: "Tip %02d%%", Int(tipPercentage*100))
         //Update total amount
-        totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = String(format: "%.2f", total)
         //Update the tip Percentage slider when a new option is clicked
         tipPercentageSlider.value = Float(tipPercentage*100)
+        splitBill.text = String(format: "%.2f", (total / numPeopleStepper.value))
     }
     
     @IBAction func tipSliderValueChanged(_ sender: Any) {
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentage = Int(tipPercentageSlider.value)
         let tip = bill * Double(tipPercentage) * 0.01
-        let total = bill + tip
+         let total = bill + tip
         //Update the percentage label
         tipAmountPercentage.text =  String(format: "Tip %02d%%", Int(tipPercentageSlider.value)
         )
         //Update tip amount label
         tipAmountField.text = String(format: "$%.2f", tip)
         //Update total amount
-        totalLabel.text = String(format: "$%.2f", total)
-
+        totalLabel.text = String(format: "%.2f", total)
+        splitBill.text = String(format: "%.2f", (total / numPeopleStepper.value))
+    }
+    
+    @IBAction func numPeopleChanged(_ sender: Any) {
+        numPeopleLabel.text = String(format: "%d people in the party", Int(numPeopleStepper.value))
+        print(numPeopleStepper.value)
+        let splitBillVal = (Double(totalLabel.text!) ?? 1.0) / Double(numPeopleStepper.value)
+        splitBill.text = String(splitBillVal)
+        
         
     }
     
